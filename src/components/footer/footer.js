301230d681a107f813a42e4697a3740ac83c434c
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2021-01-17 15:50:28 +0100

    feat: styling footer in progress

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
+import footerStyles from './footer.module.scss';
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className={footerStyles.footer}>
+            <div className={footerStyles.container}>
+                <section className={footerStyles.logos}>
+                    <h2 className={footerStyles.cm}></h2>
+                    <h2 className={footerStyles.uj}></h2>
+                </section>
+                <section>
+                    <h3>FUNDATORZY</h3>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                    <p>Katedra Filologii Romańskiej UJ-CM</p>
+                </section>
+                <section>
+                <h3>KONTAKT</h3>
+                <address>
+                    <p>ul. Ingardena</p>
+                    <p>ul. Ingardena</p>
+                    <p>
+                        <a href="mailto:brain@uj.edu.pl">brain@uj.edu.pl</a>
+                    </p>
+                    <p>
+                        <a href="tel:555555555">555 555 555</a>
+                    </p>
+                </address>
+                <div>
 
+                </div>
+                <h3>PRACA</h3>
+                </section>
+            </div>
         </footer>
     )
 };
